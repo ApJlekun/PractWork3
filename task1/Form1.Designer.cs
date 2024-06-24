cf84f426181058e357a898215b720a3987466cbc
КленинГолышев <KLENIN2000IGOR@GMAIL.COM>
2024-06-25 00:27:55 +0300

    master

---
+++ task1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.create = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // create
+            // 
+            this.create.Location = new System.Drawing.Point(179, 327);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(178, 23);
+            this.create.TabIndex = 0;
+            this.create.Text = "Создать документ по шаблону";
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.create_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(73, 68);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(270, 235);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "";
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(53, 330);
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 450);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.create);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+    }
+}
+
---
+++ task1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void create_Click(object sender, EventArgs e)
+        {
+            var doc = new Word.Application();
+            var document = doc.Documents.Add(Path.Combine(Environment.CurrentDirectory, @"C:\temp\ispp21\Шаблон.docx"));
+            doc.Visible = true;
+
+            var range = document.Paragraphs[5].Range;
+            range.Text = textBox.Text;
+
+            for (int i = 0; i < numericUpDown.Value-1; i++)
+            {
+                document.Tables[1].Rows.Add(document.Tables[1].Rows[i + 2]);
+
+                range = document.Tables[1].Cell(i + 2, 1).Range;
+                range.Text = (i + 1).ToString();
+            }
+
+            document.Content.Find.Execute(FindText: "дд.ММ.гггг", ReplaceWith: DateTime.Now, Replace: Word.WdReplace.wdReplaceAll);
+        }
+    }
+}
